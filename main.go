26f360add520134624dafa20875209f5e799c80e
mjhershfield <selmanandmatthew@gmail.com>
2020-12-09 15:45:08 -0500

    start writing a function to handle requests

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // This is the starting of the program
 func main() {
+	http.ListenAndServe(":8080", nil)
+}
 
+func handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	w.Write([]byte("hello, world!"))
 }

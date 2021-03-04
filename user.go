c3d7ec141ab7553a3a41d581542e120e58718d80
mjhershfield <selmanandmatthew@gmail.com>
2021-03-04 10:10:18 -0500

    update UserInfo to match sam's gist specifications

--- user.go
+++ user.go
@@ -1,5 +1,9 @@
 package main
 
 type UserInfo struct {
-	Username string
+	ID       string `gorm:"primaryKey" json:"id"`
+	Username string `gorm:"username" json:"username"`
+	Salt     []byte `gorm:"salt" json:"-"`
+	Rounds   uint   `gorm:"rounds" json:"-"`
+	Password []byte `gorm:"password" json:"-"`
 }
